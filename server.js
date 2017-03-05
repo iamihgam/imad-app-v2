71b25de40bb057211193d97011226d77727d9485
M. Marcus <maghimaimarcus@gmail.com>
2017-03-05 20:00:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1', function(req,res){
+app.get('/articleone', function(req,res){
     res.send('Article one will be served here');
 });
 
